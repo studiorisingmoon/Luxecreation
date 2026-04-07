ae5fe3d8f97c3cd3ba07ff998f12cd5d36f6b119
studiorisingmoon <studiorisingmoon@gmail.com>
2026-04-07 08:16:54 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+//print_r($_SERVER);die();
+
+$sitepad['db_name'] = 'mydesigndocket_7';
+$sitepad['db_user'] = 'mydesigndocket_7';
+$sitepad['db_pass'] = 'jA[8o[]JUG';
+$sitepad['db_host'] = 'localhost';
+$sitepad['db_table_prefix'] = '1zCconq_';
+$sitepad['charset'] = 'utf8mb4';
+$sitepad['collate'] = 'utf8mb4_unicode_ci';
+$sitepad['serving_url'] = 'luxecreations.in';// URL without protocol but with directory as well
+$sitepad['url'] = 'https://luxecreations.in';
+$sitepad['relativeurl'] = '';
+$sitepad['.sitepad'] = '/home2/mydesigndocket';
+$sitepad['sitepad_plugin_path'] = '/usr/local/sitepad';
+$sitepad['editor_path'] = '/usr/local/sitepad/editor';
+$sitepad['path'] = dirname(__FILE__);
+$sitepad['AUTH_KEY'] = 'jCn5SyX5imlkAUWV7ccJbgIiZgN117QpxdmmyaYv6YMEGRIvwbQdY0W7N2MabEG0';
+$sitepad['SECURE_AUTH_KEY'] = 'nKwe7lgGWxdTuTP3qR80G7pog0cCHVgJO7CeBi5YEsnT99c6OkC4T2Bm6fmlFNz2';
+$sitepad['LOGGED_IN_KEY'] = '09Ygns7msv34zoZjSK8GdrZAIUBSHXgEKEt3ob3zn9szYmuSPjJ18lQiueZGlHF0';
+$sitepad['NONCE_KEY'] = 'ideD8RDMpz5sOEeFnxOKWc0XMiXF4BHI2TeVCCJCNW0Hby5oG9YRor1Scel6UUT6';
+$sitepad['AUTH_SALT'] = 'jqbsp36GokpTRFof7vvz9a2PjKCANHWRyyOs4dCfTSTPAOcofWHENcAGv9j8tosV';
+$sitepad['SECURE_AUTH_SALT'] = 'ZxSesAaPTcvzpPCXRUVCDaj5qtEnfgirGi7ttKVo7ggz5n3w8Q2kAB7MS6uol3JK';
+$sitepad['LOGGED_IN_SALT'] = 'LsX64Kpdx5TFcxrpLcRJ7V52Ozs10VKKeYVqSJNaUTcYiZ6vOajJqDCXOrd6QjY5';
+$sitepad['NONCE_SALT'] = 'cmBnDDAxCwmDxPktnGuB8EEi7mzo8H0xAFaDAirPT0i6uCaWdra2sFibMNfZ3zkO';
+
+if(!include_once($sitepad['editor_path'].'/site-inc/bootstrap.php')){
+	die('Could not include the bootstrap.php. One of the reasons could be open_basedir restriction !');
+}
+
